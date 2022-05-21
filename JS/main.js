9fa7b52c22a259289637e5783d8d0276630634a1
reshad <you@example.com>
2022-05-21 23:28:56 +0400

    add commit

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let mydiv=document.querySelector(".container")
+    if (mydiv.style.display === 'none') {
+        mydiv.style.display = 'block';
+    } else {
+        mydiv.style.display = 'none';
+    }
+  }
